f164c8ae19d7d4282a2c3eb5298abb31235a2a70
arclogos132 <132@arclogos.com>
2021-05-19 18:02:54 +0900

    Dynamic routing with chakra ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Heading } from "@chakra-ui/react"
 import { map, bookList } from '../lib/data'
-import { getAllPostIds } from '../../lib/posts'
 
 export default function Home() {
   return (
     <>
-      {
-        map.map((data, index) => (
-          <div>
-            {data.name}
-            {data.chapters}
-          </div>
-        ))
-      }
-      {
-        bookList.map((data, index) => (
-          <div>
-            {data}
-          </div>
-        ))
-      }
+      <Heading>
+        Index to select a verse.
+      </Heading>
     </>
   )
-}
-
-export async function getStaticPaths() {
-  const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: false
-  }
 }
