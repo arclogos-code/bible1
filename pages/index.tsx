8e408229e90acd935a82a7a7fbda804f8256ff6f
arclogos132 <132@arclogos.com>
2021-05-19 23:33:02 +0900

    FL

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { Heading, Box, Button, Text, Link, Grid, GridItem } from "@chakra-ui/react"
 import { map, bookList } from '../lib/data'
 import { getAllChapterIds, getChapterData } from '../lib/chapters'
-import { List } from 'list.js'
+import dynamic from 'next/dynamic'
+import List from 'list.js'
 
 export default function Home(data) {
   return (
@@ -9,7 +10,7 @@ export default function Home(data) {
       <Heading p="6">
         Bible-slide.com
       </Heading>
-      <Grid templateColumns="repeat(5, 1fr)" gap={5} p="6">
+      <Grid className="chapterList" templateColumns="repeat(5, 1fr)" gap={5} p="6">
         {
           data.paths.map((chapter, index) => (
             <Box w="100%" h="10">
@@ -28,6 +29,7 @@ export default function Home(data) {
 }
 
 export async function getStaticProps({ params }) {
+
   const paths = getAllChapterIds()
   return {
     props: {
