5625b8d02e0e74e0d741b97c449bccadb20c0789
arclogos132 <132@arclogos.com>
2021-05-19 17:06:21 +0900

    Added krv file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { map, bookList } from '../lib/data'
 
 export default function Home() {
   return (
     <>
-      123
+      {
+        map.map((data, index) => (
+          <div>
+            {data.name}
+            {data.chapters}
+          </div>
+        ))
+      }
+      {
+        bookList.map((data, index) => (
+          <div>
+            {data}
+          </div>
+        ))
+      }
     </>
   )
 }
