17eb2d07eb13ecdf46c1f3abd20efefd4f39f9d1
arclogos132 <132@arclogos.com>
2021-05-20 23:54:57 +0900

    Update Search.tsx

--- components/Search.tsx
+++ components/Search.tsx
@@ -13,6 +13,6 @@ export default function SearchFilterInput(props) {
   };
 
   return (
-    <Input className="search" onKeyUp={handle} {...props} size="lg" fontSize="x-large" />
+    <Input className="search" onKeyUp={handle} {...props} size="lg" fontSize="x-large" autoFocus />
   )
 }
