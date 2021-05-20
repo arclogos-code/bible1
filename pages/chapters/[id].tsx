54e5ed3741f726f2d8351afdb42e56a2fe29ca41
arclogos132 <132@arclogos.com>
2021-05-20 17:01:00 +0900

    Lines to verses with dom structure

--- pages/chapters/[id].tsx
+++ pages/chapters/[id].tsx
@@ -1,11 +1,22 @@
 import { getAllChapterIds, getChapterData } from '../../lib/chapters'
 import { Box, Heading, Text } from "@chakra-ui/react"
 
-export default function Post({ chapterData }) {
+export default function Chapter({ verseList }) {
   return (
     <Box>
       <Text fontSize="3xl">
-        {chapterData.contentHtml}
+        {
+          verseList.map(({ number, verse }, index) => (
+            <Box key={number} p="6">
+              <Text>
+                {number}
+              </Text>
+              <Text>
+                {verse}
+              </Text>
+            </Box>
+          ))
+        }
       </Text>
     </Box>
   )
@@ -19,11 +30,26 @@ export async function getStaticPaths() {
   }
 }
 
+function convert(raw) {
+  raw = raw.replace(/<[^>]+>/g, '');
+  let lineList = raw.match(/\d+\W+/g);
+  let verseList = [];
+  lineList.map((line, index) => {
+    var verse = line.match(/(?<number>\d+)\s(?<text>\W*)/).groups;
+    verseList.push({
+      number: verse.number,
+      verse: verse.text.replace(/(?:\\[rn]|[\r\n]+)+/g, '')
+    })
+  });
+  return verseList
+}
+
 export async function getStaticProps({ params }) {
   const chapterData = await getChapterData(params.id)
+  const verseList = convert(chapterData.contentHtml)
   return {
     props: {
-      chapterData
+      verseList
     }
   }
 }
