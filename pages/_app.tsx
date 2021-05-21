9383bc94e99cf37fb028bdd483ea6b9f98af9f7c
arclogos132 <132@arclogos.com>
2021-05-21 14:22:51 +0900

    Meta tag og

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,11 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta charSet="euc-kr" />
         <meta title="Bible-slide.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Bible-slide.com" />
+        <meta property="og:title" content="Bible-slide.com" />
+        <meta property="og:description" content="Bible PPT" />
+        <link rel="shortcut icon" type="image/png" href="https://res.cloudinary.com/lco-linco/image/upload/b_rgb:423dd9,c_fill,g_west,h_100,w_100/v1619264171/linco_xhyhbd.png" />
       </Head>
       <Box bg="gray.900" color="white" minW="100vw" minH="100vh">
         <Component {...pageProps} />
