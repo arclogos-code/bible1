16adc8b0177c871533225c2e0f99eb5698195266
arclogos132 <132@arclogos.com>
2021-08-25 11:31:59 +0900

    Change verse with state and hash change and fullscreen

--- pages/chapters/[id].tsx
+++ pages/chapters/[id].tsx
@@ -1,10 +1,10 @@
 import { getAllChapterIds, getChapterData } from '../../lib/chapters'
-import React, { useEffect, useCallback, useRef } from 'react';
-import { VerseSlide } from '../../components/VerseSlide';
-import VerseCounter from '../../components/VerseCounter';
-import { toggleFullScreen } from '../../lib/window';
+import React, { useEffect, useCallback, useRef } from 'react'
+import { VerseSlide } from '../../components/VerseSlide'
+import VerseCounter from '../../components/VerseCounter'
+import { toggleFullScreen } from '../../lib/window'
 import { Text } from "@chakra-ui/react"
-import { convertHTMLtoVerses, getNextChapterName, getAddressFromPath, getChapterNameKRAndIndex } from '../../lib/converter';
+import { convertHTMLtoVerses, getNextChapterName, getAddressFromPath, getChapterNameKRAndIndex } from '../../lib/converter'
 
 export default function Chapter({
   verseList,
@@ -20,46 +20,65 @@ export default function Chapter({
   }
 }) {
   const verseCounter = useRef(null)
+  const [verse, setVerse] = React.useState(1)
 
-  const handleKeyDown = useCallback((event) => {
-    let hash = Number(window.location.hash.replace('#', ''))
+  const handleKeyDown = (event) => {
     // Arrow down
     if (event.keyCode === 40) {
-      if (hash < verseList.length) {
-        window.location.hash = String(hash + 1)
-        // Count up
-        verseCounter.current.setState({
-          number: hash + 1
-        });
-        event.preventDefault();
+      if (verse < verseList.length) {
+        setVerse(verse + 1)
+        event.preventDefault()
       } else {
+
         // Go to next chapter or book.
-        document.removeEventListener("keydown", handleKeyDown, false);
+        document.removeEventListener("keydown", handleKeyDown, false)
         window.location.href = '/chapters/' + getNextChapterName(getAddressFromPath(window.location.pathname))
-        event.preventDefault();
+        event.preventDefault()
       }
+
     } else if (event.keyCode === 38) {
       // Arrow up
-      if (hash > 1) {
-        window.location.hash = String(hash - 1)
-        // Count down
-        verseCounter.current.setState({
-          number: hash - 1
-        });
-        event.preventDefault();
+      if (verse > 1) {
+        setVerse(verse - 1)
+        event.preventDefault()
       }
+
     } else if (event.keyCode == 13) {
-      toggleFullScreen();
-      return false;
+      toggleFullScreen()
+      setTimeout(() => {
+        var target = document.getElementById(String(verse))
+        window.scrollTo(0, target.scrollHeight * (verse - 1))
+      }, 300)
+      return false
     }
-  }, []);
+  }
+
+  const hashChange = () => {
+    if (window.location.hash) {
+      const hash = Number(window.location.hash.replace('#', ''))
+      setVerse(hash)
+    } else {
+      setVerse(1)
+    }
+  }
 
   useEffect(() => {
-    if (!window.location.hash) {
-      window.location.hash = '#1'
+    hashChange()
+  }, [])
+
+  useEffect(() => {
+    window.location.hash = String(verse)
+    verseCounter.current.setState({
+      number: verse
+    })
+
+    document.addEventListener("keydown", handleKeyDown, false)
+    window.addEventListener("hashchange", hashChange, false)
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown, false)
+      window.removeEventListener("hashchange", hashChange, false)
     }
-    document.addEventListener("keydown", handleKeyDown, false);
-  }, []);
+  }, [verse])
 
   return (
     <>
@@ -68,9 +87,15 @@ export default function Chapter({
           <VerseSlide props={{ number, verse }} key={number} />
         ))
       }
-      <Text onClick={() => { window.location.href = '/' }}
-        position="fixed" right="7vh" bottom="5vh" fontSize="4vh" fontWeight="semibold" cursor="pointer">
-        {data.nameKR + ' ' + data.chapterIndex}:<VerseCounter ref={verseCounter}></VerseCounter>
+      <Text
+        onClick={() => { window.location.href = '/' }}
+        position="fixed"
+        right="10vh"
+        bottom="7vh"
+        fontSize="5vh"
+        fontWeight="semibold"
+        cursor="pointer">
+        {data.nameKR + ' ' + data.chapterIndex}:<VerseCounter ref={verseCounter} />
       </Text>
     </ >
   )
