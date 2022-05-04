4b6b22d292689a174581f64e164c73ee3f6f00e4
arclogos132 <54137057+arclogos132@users.noreply.github.com>
2022-05-04 14:57:33 +0900

    Update VerseSlide.tsx

--- components/VerseSlide.tsx
+++ components/VerseSlide.tsx
@@ -1,37 +1,41 @@
-import { Flex, Text } from "@chakra-ui/react"
-import React from 'react';
+/** @format */
+
+import { Flex, Text } from '@chakra-ui/react'
+import React from 'react'
 
 export const VerseSlide = ({
   props
 }: {
   props: {
-    number: string,
+    number: string
     verse: string
   }
 }) => {
-  return <Flex
-    id={props.number}
-    className="verse"
-    p={6}
-    v="100vw"
-    h="100vh"
-    justifyContent="center"
-    alignItems="flex-start">
-    <Flex mt="5vh" fontWeight="semibold">
-      <Text
-        h="fit-content"
-        fontSize={{ base: 'x-large', md: '5vh' }}
-        mr={{ base: '5', md: '5vh' }}
-        mt={{ base: '1', md: '1.3vh' }}>
-        {props.number}
-      </Text>
-      <Text
-        h="fit-content"
-        fontSize={{ base: 'x-large', md: '6.7vh' }}
-        w="80vw"
-        lineHeight="tall">
-        {props.verse}
-      </Text>
+  return (
+    <Flex
+      id={props.number}
+      className='verse'
+      p={6}
+      w='100vw'
+      h='100vh'
+      justifyContent='center'
+      alignItems='flex-start'>
+      <Flex mt='5vh' fontWeight='semibold'>
+        <Text
+          h='fit-content'
+          fontSize={{ base: 'x-large', md: '5vh' }}
+          mr={{ base: '5', md: '5vh' }}
+          mt={{ base: '1', md: '1.3vh' }}>
+          {props.number}
+        </Text>
+        <Text
+          h='fit-content'
+          fontSize={{ base: 'x-large', md: '6.7vh' }}
+          w='80vw'
+          lineHeight='tall'>
+          {props.verse}
+        </Text>
+      </Flex>
     </Flex>
-  </Flex>
+  )
 }
